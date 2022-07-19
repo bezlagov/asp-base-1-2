c5bd5943886f0f570daa93b82e9e83e975fa5142
Ivan Zhyhalkin <ivan.zhigalkin@gmail.com>
2022-07-19 07:46:35 +0300

    Add project files.

---
+++ SimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Download()
+        {
+            return File("~/introduction.pdf", "application/pdf");
+        }
+    }
+}
